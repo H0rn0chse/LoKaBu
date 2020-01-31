94437f557965513d4d7ebd055415994e75da668f
H0rn0chse <h0rn0chse@gmx.de>
2020-01-31 20:54:56 +0100

    Fixed eslint

--- assets/navigation.js
+++ assets/navigation.js
@@ -1,31 +1,31 @@
 document.body.addEventListener('click', (oEvent) => {
-	if (oEvent.target.dataset.section) {
-		navigateToSection(oEvent.target.dataset.section)
-	}
-})
+    if (oEvent.target.dataset.section) {
+        navigateToSection(oEvent.target.dataset.section);
+    }
+});
 
 function hideAllSectionsAndDeselectButtons () {
-	const aSections = document.querySelectorAll('.section.is-shown')
-	aSections.forEach((oSection) => {
-		oSection.classList.remove('is-shown')
-	})
-  
-	const aButtons = document.querySelectorAll('.nav-item.is-selected')
-	aButtons.forEach((oButton) => {
-		oButton.classList.remove('is-selected')
-	})
+    const aSections = document.querySelectorAll('.section.is-shown');
+    aSections.forEach((oSection) => {
+        oSection.classList.remove('is-shown');
+    });
+
+    const aButtons = document.querySelectorAll('.nav-item.is-selected');
+    aButtons.forEach((oButton) => {
+        oButton.classList.remove('is-selected');
+    });
 }
 
 function navigateToSection (sSection) {
-	const sSectionId = "section-" + sSection
-	const oSectionController = window[sSection + "Section"];
+    const sSectionId = "section-" + sSection;
+    const oSectionController = window[sSection + "Section"];
 
-	if (!document.querySelectorAll("#" + sSectionId + ".is-shown").length) {
-		hideAllSectionsAndDeselectButtons()
-		document.getElementById(sSectionId).classList.add('is-shown')
-		oSectionController.init();
-	}
+    if (!document.querySelectorAll("#" + sSectionId + ".is-shown").length) {
+        hideAllSectionsAndDeselectButtons();
+        document.getElementById(sSectionId).classList.add('is-shown');
+        oSectionController.init();
+    }
 }
 
-//default section
-navigateToSection("new")
+// default section
+navigateToSection("new");
