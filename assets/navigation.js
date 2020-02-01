9f73cdfe35a6088410358714cdcb209f87040990
H0rn0chse <h0rn0chse@gmx.de>
2020-02-01 14:31:20 +0100

    Improved navHeaderBar with color cursor and highlighting

--- assets/navigation.js
+++ assets/navigation.js
@@ -1,18 +1,23 @@
 document.body.addEventListener('click', (oEvent) => {
-    if (oEvent.target.dataset.section) {
+    if (oEvent.target.classList.contains("nav-item")) {
+        var oNavLink = oEvent.target.querySelectorAll(".nav-link[data-section]")[0];
+        if (oNavLink) {
+            navigateToSection(oNavLink.dataset.section);
+        }
+    } else if (oEvent.target.dataset.section) {
         navigateToSection(oEvent.target.dataset.section);
     }
 });
 
 function hideAllSectionsAndDeselectButtons () {
-    const aSections = document.querySelectorAll('.section.is-shown');
+    const aSections = document.querySelectorAll(".section.is-shown");
     aSections.forEach((oSection) => {
-        oSection.classList.remove('is-shown');
+        oSection.classList.remove("is-shown");
     });
 
-    const aButtons = document.querySelectorAll('.nav-item.is-selected');
-    aButtons.forEach((oButton) => {
-        oButton.classList.remove('is-selected');
+    const aNavItems = document.querySelectorAll(".nav-item");
+    aNavItems.forEach((oNavItem) => {
+        oNavItem.classList.remove("is-shown");
     });
 }
 
@@ -23,8 +28,21 @@ function navigateToSection (sSection) {
     if (!document.querySelectorAll("#" + sSectionId + ".is-shown").length) {
         hideAllSectionsAndDeselectButtons();
         document.getElementById(sSectionId).classList.add('is-shown');
+        const aNavItems = document.querySelectorAll(".nav-item");
+        aNavItems.forEach((oNavItem) => {
+            if (hasChildByCss(oNavItem, ".nav-link[data-section=" + sSection + "]")) {
+                oNavItem.classList.add("is-shown");
+            }
+        });
         oSectionController.init();
     }
+}
+
+function hasChildByCss (oNode, sSelector) {
+    if (oNode.querySelectorAll(sSelector).length) {
+        return true;
+    }
+    return false;
 }
 
 // default section
