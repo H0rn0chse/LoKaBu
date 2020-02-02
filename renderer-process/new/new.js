cefc43f6128f64df6d78b8c033b7a4757131a8a2
H0rn0chse <h0rn0chse@gmx.de>
2020-02-02 16:57:19 +0100

    removed database tests

--- renderer-process/new/new.js
+++ renderer-process/new/new.js
@@ -1,48 +1,8 @@
-const oAccounts = require("../databaseObjects/accounts");
-const oDatabaseInfo = require("../databaseObjects/databaseInfo");
-const oPersons = require("../databaseObjects/persons");
-const oReceiptAnalysis = require("../databaseObjects/receiptAnalysis");
-const oReceiptDetail = require("../databaseObjects/receiptDetail");
-const oReceiptList = require("../databaseObjects/receiptList");
-const oSettings = require("../databaseObjects/settings");
-const oTypes = require("../databaseObjects/types");
-
 window.newSection = {
     init: function () {
         console.log("Section new was initialized");
-        oAccounts.refresh(() => {
-            console.log("accounts:", oAccounts.get());
-        });
-        oDatabaseInfo.refresh(() => {
-            console.log("databaseInfo", oDatabaseInfo.get());
-        });
-        oPersons.refresh(() => {
-            console.log("persons", oPersons.get());
-        });
-        oReceiptAnalysis.refresh(() => {
-            console.log("receiptAnalysis", oReceiptAnalysis.get());
-        });
-        oReceiptDetail.refresh(() => {
-            console.log("receiptDetail", oReceiptDetail.get());
-        });
-        oReceiptList.refresh(() => {
-            console.log("receiptList", oReceiptList.get());
-        });
-        oSettings.refresh(() => {
-            console.log("setings", oSettings.get());
-        });
-        oTypes.refresh(() => {
-            console.log("types", oTypes.get());
-        });
     },
-    readConfig: function () {
-        console.log("accounts:", oAccounts.get());
-        console.log("databaseInfo", oDatabaseInfo.get());
-        console.log("persons", oPersons.get());
-        console.log("receiptAnalysis", oReceiptAnalysis.get());
-        console.log("receiptDetail", oReceiptDetail.get());
-        console.log("receiptList", oReceiptList.get());
-        console.log("setings", oSettings.get());
-        console.log("types", oTypes.get());
+    test: function () {
+        console.log("test");
     }
 };
