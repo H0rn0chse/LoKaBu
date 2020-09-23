4b2e31cffd89749917dfe498adc09517b98c1e22
H0rn0chse <h0rn0chse@gmx.de>
2020-09-23 21:15:40 +0200

    Added view_Analysis to database

---
+++ worker/migration/upgrades/3.1_3.2.sql
@@ -0,0 +1,26 @@
+PRAGMA foreign_keys=off;
+BEGIN TRANSACTION;
+
+-- Update version
+UPDATE Settings
+SET Version = '3.2';
+
+/*================================================================================
+    Restore Views
+================================================================================*/
+CREATE VIEW view_Analysis AS
+    SELECT
+        strftime('%Y-%m', r.Date, 'unixepoch') as Date,
+        r.Account as SourceAccount,
+        r.Comment as Comment,
+        r.Store as Store,
+        l.Value as Value,
+        l.Account as TargetAccount,
+        l.Type as Type,
+        l.Billing as Person
+    FROM Lines l
+        LEFT JOIN Receipts r
+            ON r.ID = l.Receipt
+
+COMMIT;
+PRAGMA foreign_keys=on;
