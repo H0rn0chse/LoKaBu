b51f44b011a000c4aa614e94eccc5ab11252eb01
H0rn0chse <h0rn0chse@gmx.de>
2020-11-22 12:35:50 +0100

    Enabled receipt-summary

--- worker/database/receiptList.js
+++ worker/database/receiptList.js
@@ -2,6 +2,7 @@ import { SqlStatement } from "../common/SqlStatement.js";
 import { Table } from "../common/Table.js";
 import { SqlFilterOption } from "../common/SqlFilterOption.js";
 import { DatabaseManager } from "./DatabaseManager.js";
+import { EventBus } from "../../renderer/EventBus.js";
 
 class _ReceiptListView extends Table {
     constructor () {
@@ -11,6 +12,14 @@ class _ReceiptListView extends Table {
         SELECT *
         FROM view_ReceiptList
         `;
+
+        EventBus.listen("receiptList-duplicates", () => {
+            EventBus.sendToBrowser("receiptList-duplicates", this.findDuplicates());
+        });
+
+        EventBus.listen("receipt-summary", (oEvent, sId) => {
+            EventBus.sendToBrowser("receipt-summary", this.readReceipt(sId));
+        });
     }
 
     readSqlAction (aFilter = [], iPage = 1, sSortColumn = "ID", sSortDirection = "ASC") {
@@ -31,6 +40,103 @@ class _ReceiptListView extends Table {
             CurrentPage: iPage,
             PageCount: aEntries.length > 0 ? Math.ceil(aEntries[0].LineCount / aEntries[0].PageSize) : 1
         };
+    }
+
+    readReceipt (sId) {
+        const sSql = `
+        SELECT
+            view.*,
+            json_group_array(l.value) as Lines
+        FROM view_ReceiptList view
+        JOIN Lines l
+            ON view.ID = l.Receipt
+        WHERE view.ID = ${sId}
+        `;
+        return DatabaseManager.get()
+            .prepare(sSql)
+            .get();
+    }
+
+    findDuplicates () {
+        const oDb = DatabaseManager.get();
+        oDb.function("evalDuplicate", { deterministic: true, varargs: true }, this.evalDuplicate);
+
+        let sSql = `
+        SELECT
+            json_group_array(DISTINCT ID) as ID,
+            evalDuplicate(
+                $ReceiptSum,
+                ReceiptSum,
+                $Date,
+                Date,
+                $Account,
+                Account,
+                $Store,
+                Store
+            ) as count
+        FROM view_ReceiptList
+        WHERE count > 1
+            AND $ID != ID;
+        `;
+        const oDuplicateStatement = oDb.prepare(sSql);
+
+        sSql = `
+        SELECT
+            view.*,
+            r.DuplicateHint
+        FROM view_ReceiptList view
+        JOIN Receipts r
+            ON r.ID = view.ID
+        `;
+        const oStmt = oDb.prepare(sSql);
+
+        const aResult = [];
+        for (const oItem of oStmt.iterate()) {
+            const oResult = oDuplicateStatement.get(oItem);
+            oResult.ID = JSON.parse(oResult.ID);
+
+            const aDuplicateList = JSON.parse(oItem.DuplicateHint || "[]");
+            oResult.ID = oResult.ID.filter(sID => {
+                return !aDuplicateList.includes(sID);
+            });
+
+            if (oResult.ID.length) {
+                oResult.ID.push(oItem.ID);
+                oResult.ID.sort();
+
+                const sResult = JSON.stringify(oResult);
+                if (!aResult.includes(sResult)) {
+                    aResult.push(sResult);
+                }
+            }
+        }
+        return aResult.map(sItem => {
+            return JSON.parse(sItem);
+        });
+    }
+
+    evalDuplicate (iSum1, iSum2, iDate1, iDate2, iAccount1, iAccount2, iStore1, iStore2) {
+        let iCount = 0;
+
+        if (iSum1 === iSum2) {
+            iCount++;
+
+            const iDays = 2;
+            const iSeconds = 60 * 60 * 24 * iDays;
+            if (iDate1 - iSeconds < iDate2 && iDate1 + iSeconds > iDate2) {
+                iCount++;
+
+                if (iAccount1 === iAccount2) {
+                    iCount++;
+                }
+
+                if (iStore1 === iStore2) {
+                    iCount++;
+                }
+            }
+        }
+
+        return iCount;
     }
 }
 
