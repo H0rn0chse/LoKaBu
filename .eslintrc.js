dcb4c7615d1913bf5e12abca903f2d93b87b391c
H0rn0chse <h0rn0chse@gmx.de>
2020-12-22 13:24:28 +0100

    Updated ECMA version for ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,7 @@ module.exports = {
         SharedArrayBuffer: 'readonly'
     },
     parserOptions: {
-        ecmaVersion: 2018
+        ecmaVersion: 2020
     },
     rules: {
         "prefer-promise-reject-errors": 0,
