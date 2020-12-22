3802043a6d24838b00508e7db3e0887d814e7e32
H0rn0chse <h0rn0chse@gmx.de>
2020-12-22 13:36:33 +0100

    Added test view and test component

---
+++ core/view/Node.js
@@ -0,0 +1,45 @@
+import { Component } from "./Component.js";
+import { DomElement } from "./DomElement.js";
+
+export class Node extends Component {
+    constructor (oAttributes) {
+        super(oAttributes);
+
+        this.properties = ["childNodes", "type", "class", "value", "text"];
+        this.events = ["change"];
+        this.element = null;
+    }
+
+    render () {
+        if (!this.element) {
+            this.element = new DomElement("div");
+        }
+        this.element.appendToParent(this.getParentDomRef());
+        //this.node.remove();
+        // TODO how to handle rerendering
+    }
+
+    setChildNodes (value) {
+
+    }
+
+    setType (value) {
+
+    }
+
+    setClass (value) {
+        this.element.addClass(value);
+    }
+
+    setValue (value) {
+
+    }
+
+    setText (value) {
+        this.element.setText(value);
+    }
+
+    getDomRef () {
+        return this.element.getNode();
+    }
+}
---
+++ renderer/view/test/TestView.js
@@ -0,0 +1,9 @@
+import { XMLView } from "../../../core/view/XMLView.js";
+
+export class TestView extends XMLView {
+    constructor (models) {
+        super(models, "renderer/view/test/TestView");
+    }
+
+    update () {}
+};
